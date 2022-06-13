0982282f3413bb468215334148b70a952cd946ae
Deepak Jha <djha@akamai.com>
2022-06-13 13:39:49 +1000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM akamai/shell
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -o pipefail
+
+# Create /root/.edgerc file from env variable
+echo -e "${EDGERC}" > '$HOME/.edgerc'
+
+#  Set Variables
+propertyName=$1
+
+
+# check if propertyName exists
+if [ -f ${propertyName}] ; then
+  echo "Property name defined..this github action will proceed"
+  mycommand1="akamai terraform --accountkey ${{secrets.AKAMAI_AS_ACCOUNTSWITCHKEY}} create-property ${propertyName}"
+  echo "Running: $mycommand1"
+  eval $mycommand1 > output1
+  status="cat output1 | grep -i Fail"
+    if  [-f $status] ; then
+  echo "Operation failed with error..."
+    fi
+else
+  echo -e "Error: ${propertyName} is missing.\nYou may want to define ${propertyName} into the workflow\n"
+  exit 123
+fi
