bf31e0ccd64a48c06af703bd92a8811680d02126
Deepak Jha <djha@akamai.com>
2022-06-13 17:24:37 +1000

    v1.72

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ echo -e "${EDGERC}" > ~/.edgerc
 #  Set Variables
 propertyName=$1
 
-mycommand="akamai terraform --accountkey $secrets.AKAMAI_AS_ACCOUNTSWITCHKEY create-property $propertyName"
+mycommand="akamai terraform --accountkey $SWITCH_KEY create-property $propertyName"
 eval $mycommand > output
 cat output
+status=$(cat output | grep 'statusCode' | awk '{print $2}' | tr -d ,)
+if [ $status -eq 401 ] ; then
+  echo "import command failed with: $output"
+  exit 123
+else
+  echo "success"
+fi
