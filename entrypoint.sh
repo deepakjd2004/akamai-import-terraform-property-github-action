c7f22b874da19aaf87ce4b69d792636eddb4817f
Deepak Jha <djha@akamai.com>
2022-06-13 16:37:35 +1000

    v1.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,4 @@ echo -e "${EDGERC}" > ~/.edgerc
 #  Set Variables
 propertyName=$1
 
-
-# check if propertyName exists
-if [ -z "${propertyName}" ] ; then
-  echo "Property name defined..this github action will proceed"
-  mycommand1="akamai terraform --accountkey ${{secrets.AKAMAI_AS_ACCOUNTSWITCHKEY}} create-property ${propertyName}"
-  echo "Running: $mycommand1"
-  eval $mycommand1 > output1
-  status="cat output1 | grep -i Fail"
-    if  [-f $status] ; then
-  echo "Operation failed with error..."
-    fi
-else
-  echo -e "Error: ${propertyName} is missing.\nYou may want to define ${propertyName} into the workflow\n"
-  exit 123
-fi
+echo "akamai terraform --accountkey ${{secrets.AKAMAI_AS_ACCOUNTSWITCHKEY}} create-property ${propertyName}"
