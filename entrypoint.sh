e3a2731baf58ed3791f3921345da87923a2a0138
Deepak Jha <djha@akamai.com>
2022-06-13 17:44:23 +1000

    v1.75

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ set -o pipefail
 # Create /root/.edgerc file from env variable
 echo -e "${EDGERC}" > ~/.edgerc
 
-#  Set Variables
+# SetVariables
 propertyName=$1
 
 mycommand="akamai terraform --accountkey $SWITCH_KEY create-property $propertyName"
 eval $mycommand > output
 cat output
 status=$(cat output | grep 'statusCode' | awk '{print $2}' | tr -d ,)
-if [ "$status" -eq 401 ] ; then
+if [ "$status" -eq "401" ] ; then
   echo "import command failed with: $output"
   exit 123
 else
